c990741dc6717d57136f75c11afee78d89803754
Philip Krause <philipkrause7@gmail.com>
2023-09-21 13:11:57 +0200

    Changes unrelated to a JIRA task

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,4 @@
 export const GAME_WIDTH = 480;
 export const GAME_HEIGHT = 800;
+export const GAME_SPEED = 2;
 export const DEBUG_MODE = false
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,3 +1,4 @@
+import * as Config from '../config/constants';
 
 class GameScene extends Phaser.Scene {
   constructor(config) {
@@ -9,7 +10,8 @@ class GameScene extends Phaser.Scene {
     this.createHorse;
     this.horse1;
     this.realTime = 0;
-    this.realtimeText;
+    this.realtimeText;  
+    this.gameSpeed = Config.GAME_SPEED;
   }
   
   //PRELOAD===================================================================================
@@ -19,6 +21,9 @@ class GameScene extends Phaser.Scene {
     this.load.image('background1_seats', '../assets/images/background1_seats.png');
     this.load.image('background1_course', '../assets/images/background1_course.png');
     this.load.image('background1_wall', '../assets/images/background1_wall.png');
+    this.load.image('background1_trees', '../assets/images/background1_trees.png');
+    this.load.image('background1_fence', '../assets/images/background1_fence.png');
+
     this.load.spritesheet('horse1','../assets/images/horse1_sheet.png', { frameWidth: 432, frameHeight: 321 }); 
     this.load.spritesheet('horse2','../assets/images/horse2_sheet.png', { frameWidth: 432, frameHeight: 321 }); 
     this.load.spritesheet('horse3','../assets/images/horse3_sheet.png', { frameWidth: 432, frameHeight: 321 }); 
@@ -33,9 +38,12 @@ class GameScene extends Phaser.Scene {
     const width = this.scale.width;
     const height = this.scale.height;
     this.background_clouds = this.add.tileSprite(width ,243,0,0,'background1_clouds').setDepth(1);
-    this.background_wall = this.add.tileSprite(0 ,360,0,0,'background1_wall').setDepth(1);
-    this.background_seats = this.add.tileSprite(0,300,0,0,'background1_seats').setDepth(4);
+    this.background_wall = this.add.tileSprite(0 ,360,0,0,'background1_wall').setDepth(2);
+    this.background_trees = this.add.tileSprite(0 ,330,0,0,'background1_trees').setDepth(4);
+    this.background_seats = this.add.tileSprite(0,292,0,0,'background1_seats').setDepth(3);
     this.background_course = this.add.tileSprite(0,492,0,0,'background1_course').setDepth(3);
+    this.background_fence = this.add.tileSprite(0,365,0,0,'background1_fence').setDepth(3);
+    this.background_fence2 = this.add.tileSprite(0,602,0,0,'background1_fence').setDepth(3);
 
 
     //text
@@ -55,8 +63,8 @@ class GameScene extends Phaser.Scene {
       this.tweens.add({
         targets: [sprite],
         x: 300,
-        duration: 8000,
-        hold: 500,
+        duration: 10000,
+        hold: 700,
         loop: -1,
         loopDelay: delay,
         ease: 'back.inout',
@@ -71,7 +79,6 @@ class GameScene extends Phaser.Scene {
     this.horse5 = this.physics.add.sprite(width * .5 -100,this.horse1.y + 155 ,'horse2').setScale(.3).setDepth(4);
 
 
-    
     this.horses = [
       {horse: this.horse1, speed: 3, stamina: 1},
       {horse: this.horse2, speed: 1, stamina: 1},
@@ -94,16 +101,24 @@ class GameScene extends Phaser.Scene {
 
     this.realTime += 1/60
 
-    // let rng = Phaser.Math.Between(0,150)
-    // console.log(rng)
+    if (Math.abs(this.realTime) %2 === 0){
+      let rng = Phaser.Math.Between(0,150)
+      console.log(`RNG:${rng}`)
+    }
     this.horses.forEach((h,index) => {
       h.horse.anims.play(`horse${index+1}-run`,true)
     })
 
+
+    //background motion
     this.realtimeText.setText(`time: ${this.realTime.toFixed(2)}`)
-    this.background_clouds.tilePositionX += 1
-    this.background_seats.tilePositionX += 3
-   // this.background.tilePositionX += 8
+    this.background_clouds.tilePositionX += .5 * this.gameSpeed
+    this.background_seats.tilePositionX += 2 * this.gameSpeed
+    this.background_trees.tilePositionX += 3 * this.gameSpeed
+    this.background_wall.tilePositionX += 3 * this.gameSpeed
+    this.background_fence.tilePositionX += 4 * this.gameSpeed;
+    this.background_fence2.tilePositionX += 4 * this.gameSpeed;
+
   }
 
 
