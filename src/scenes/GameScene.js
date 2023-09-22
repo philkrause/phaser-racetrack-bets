3cc026a434723c5a9090bb01943318a4a9f0a3d0
Philip Krause <philipkrause7@gmail.com>
2023-09-22 10:54:54 +0200

    Changes unrelated to a JIRA task

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -27,6 +27,10 @@ class GameScene extends Phaser.Scene {
     this.load.image('background1_wall', '../assets/images/background1_wall.png');
     this.load.image('background1_trees', '../assets/images/background1_trees.png');
     this.load.image('background1_fence', '../assets/images/background1_fence.png');
+    this.load.image('gui_background', '../assets/images/gui_background.png');
+    this.load.image('button_pause', '../assets/images/button_pause.png');
+
+
 
     this.load.spritesheet('horse0', '../assets/images/horse1_sheet.png', {
       frameWidth: 432,
@@ -63,6 +67,8 @@ class GameScene extends Phaser.Scene {
     this.background_course = this.add.tileSprite(0, 492, 0, 0, 'background1_course').setDepth(3);
     this.background_fence = this.add.tileSprite(0, 365, 0, 0, 'background1_fence').setDepth(3);
     this.background_fence2 = this.add.tileSprite(0, 602, 0, 0, 'background1_fence').setDepth(3);
+    this.gui_background = this.add.image(width*.5, 633, 'gui_background').setScale(.432);
+    this.button_pause = this.add.image(width-20, 633, 'button_pause').setScale(.5);
 
     this.allBackgrounds = [
       {
@@ -92,6 +98,8 @@ class GameScene extends Phaser.Scene {
         tileSpeed: 4,
       },
     ]
+
+    
     //text
     this.realtimeText = this.add.bitmapText(10, 10, 'ccFont', '').setTint(this.red).setScale(.5).setScrollFactor(0).setDepth(2)
 
@@ -116,12 +124,12 @@ class GameScene extends Phaser.Scene {
         hold: 700,
         loopDelay: delay,
         onComplete: () => {
-          console.log('dash completed')
           sprite.dashing = false
         },
         yoyo: true,
       })
     }
+
 
     this.horse0 = this.physics.add.sprite(width * .5 - 100, (height * .5) - 20, 'horse0').setScale(.3).setDepth(4);
     this.horse1 = this.physics.add.sprite(width * .5 - 100, this.horse0.y + 40, 'horse1').setScale(.3).setDepth(4);
@@ -132,30 +140,35 @@ class GameScene extends Phaser.Scene {
 
     this.horses = [{
         horse: this.horse0,
+        name: 'Viscount',
         dashing: false,
         speed: 3,
         stamina: 1
       },
       {
         horse: this.horse1,
+        name: 'Lady Grace',
         dashing: false,
         speed: 1,
         stamina: 1
       },
       {
         horse: this.horse2,
+        name: 'Princess April',
         dashing: false,
         speed: 1,
         stamina: 1
       },
       {
         horse: this.horse3,
+        name: 'Izzy',
         dashing: false,
         speed: 1,
         stamina: 1
       },
       {
         horse: this.horse4,
+        name: 'Lord Dorian',
         dashing: false,
         speed: 1,
         stamina: 1
@@ -178,6 +191,7 @@ class GameScene extends Phaser.Scene {
     
     this.createDashTimer += 1/60
 
+    //create Dash
     if(this.createDashTimer >= 2){
       let rng = Phaser.Math.Between(0,this.horses.length-1)
       
@@ -185,7 +199,7 @@ class GameScene extends Phaser.Scene {
       let horseDashing = this.horses[rng].dashing;
 
       if(!horseDashing){
-        console.log(`Horse ${rng} Dashing!`)
+        //console.log(`Horse ${this.horses[rng].name} Dashing!`)
         
         this.horses[rng].dashing = true;
         this.createDash(horse, 1000)
@@ -194,6 +208,7 @@ class GameScene extends Phaser.Scene {
       
     }
 
+    //text
     this.realtimeText.setText(`time: ${this.realTime.toFixed(2)}`)
 
   
@@ -201,13 +216,15 @@ class GameScene extends Phaser.Scene {
     this.horses.forEach((h, index) => {
       h.horse.anims.play(`horse${index}-run`, true)
     })
-
+    
+    
 
     //background motion
     this.allBackgrounds.forEach(b => {
      b.background.tilePositionX += b.tileSpeed * this.gameSpeed
     })
 
+    //console.log(this.allBackgrounds[0].background.tilePositionX)
   }
 
 
